d592b8359fbf9d36f39cb999164cdb8a45b4d678
Rafael Ch <rafael.cherman0@gmail.com>
2023-09-16 17:14:06 -0300

    pedido teste finalizado

--- src/test/java/com/example/pizzaria/PedidoTeste.java
+++ src/test/java/com/example/pizzaria/PedidoTeste.java
@@ -1,6 +1,7 @@
 package com.example.pizzaria;
 
 import com.example.pizzaria.controller.PedidoController;
+import com.example.pizzaria.dto.*;
 import com.example.pizzaria.entity.*;
 import com.example.pizzaria.repository.PedidoRepository;
 import org.junit.Assert;
@@ -51,7 +52,7 @@ public class PedidoTeste {
         Pizza pizza = new Pizza(pizzaTipo, sabor);
         pizza.setId(1l);
         pizza.setAtivo(true);
-        List<Pizza> pizzas = new ArrayList<>();
+        Set<Pizza> pizzas = new HashSet<>();
         pizzas.add(pizza);
         Set<ProdutoDiverso> produtos = new HashSet<>();
         produtos.add(produtoDiverso);
@@ -66,6 +67,7 @@ public class PedidoTeste {
 
         Mockito.when(repositorio.findById(1l)).thenReturn(Optional.of(pedido));
         Mockito.when(repositorio.findAll()).thenReturn(pedidos);
+        Mockito.when(repositorio.doesExist(1l)).thenReturn(true);
     }
 
     @Test
@@ -84,18 +86,47 @@ public class PedidoTeste {
     @Test
     void testeCadastrar()
     {
-
+        PedidoDTO pedidoDTO = criaPedido();
+        var teste = controller.cadastrar(pedidoDTO);
+        Assert.assertTrue(teste.getBody().contains("cadastrado"));
     }
 
     @Test
     void testeEditar()
     {
-
+        PedidoDTO pedidoDTO = criaPedido();
+        var teste = controller.editar(1l, pedidoDTO);
+        Assert.assertTrue(teste.getBody().contains("alterado"));
     }
 
     @Test
     void testeDeletar()
     {
+        var teste = controller.deletar(1l);
+        Assert.assertTrue(teste.getBody().contains("desativado"));
+    }
+
+    private PedidoDTO criaPedido()
+    {
+        ProdutoDiversoDTO produtoDiverso = new ProdutoDiversoDTO("coca-cola", "refrigerante", 1, 4.99);
+        ClienteDTO cliente = new ClienteDTO("Lucas","99 99999-9999", "999.999.999-99");
+        EnderecoDTO endereco = new EnderecoDTO("8888-8888","Carmin",777,"Neves","88888-888","casa",cliente);
+        FuncionarioDTO atendente = new FuncionarioDTO("Cauan", "777.777.777-77", "Atendente");
+        FuncionarioDTO entregador = new FuncionarioDTO("Vitor", "888.888.888-88", "Entregador");
+        SaborDTO sabor = new SaborDTO("Milho","Milho, Queijo, Massa de tomate", 1.99);
+        PizzaTipoDTO pizzaTipo = new PizzaTipoDTO("pizza", "pequeno", 35);
+        PizzaDTO pizza = new PizzaDTO(pizzaTipo, sabor);
+        Set<PizzaDTO> pizzas = new HashSet<>();
+        pizzas.add(pizza);
+        Set<ProdutoDiversoDTO> produtos = new HashSet<>();
+        produtos.add(produtoDiverso);
+        Date data = new Date();
+
+        PedidoDTO pedido = new PedidoDTO(cliente,atendente,endereco,entregador,true,"pedido",50,"dinheiro",false,data,pizzas,produtos);
+        pedido.setId(1l);
+        pedido.setAtivo(true);
+        return pedido;
 
     }
+
 }
